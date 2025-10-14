43b1aeb74015eac5b7b538312a4b19f1038fce2a
Mateus Souza <mateus_msouza@outlook.com>
2025-10-13 21:31:05 -0300

    update

--- graficos/faces_oldschool/loss_benchmark.py
+++ graficos/faces_oldschool/loss_benchmark.py
@@ -29,3 +29,40 @@ all_dfs = [
     _create_exp_dict(
         "Vanilla with Mixed Faces", "./runs_loss/efficientNetb0_classify_by_faces_BASELINE_6T_1_fold-1_Loss_train.csv"),
 ]
+
+
+df = {
+    # "Step": pd.read_csv(curves[0])["Step"],
+    "FaceMixup": face_mix["Value"],
+    "MixAugment": mixedupaugment["Value"],
+    "CutMix": cutmix["Value"],
+    "Random Erasing": baseline_random_erasing["Value"],
+    "MixUp": mixup["Value"],
+    "Vanilla": baseline_0t["Value"],
+    "Vanilla with Mixed Faces": baseline_6t["Value"]
+}
+df = pd.DataFrame(df)
+sns.set_theme(style='white', font_scale=1)
+sns.color_palette("rocket_r")
+
+plt.figure(figsize=(8, 6))
+facemixup_color = r"#3357FF"  # 4990c2"
+facemixup_rs_color = r"#ff8e2b"
+mixaugment_color = r"#f02041"
+cutmix_color = r"#F39C12"
+random_erasing_color = r"#8E44AD"
+mixup = r"#2ECC71"
+vanilla_6k = r"#9d402f"
+vanilla = "#7a9e2f"
+custom_palette = [facemixup_color, mixaugment_color,
+                  cutmix_color, random_erasing_color, mixup, vanilla_6k, vanilla]
+
+ax = sns.lineplot(data=df, sort=True, palette=custom_palette)
+ax.set(xlabel='Epoch', ylabel='Accuracy')
+
+
+plt.title("Approaches")
+plt.legend()
+plt.grid(False)
+plt.tight_layout()
+plt.savefig("approaches_losses.png")
